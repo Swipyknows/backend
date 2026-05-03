1272174363babeacd20d9de766ad520358aa603f
sweepyknows <psr90889@gmail.com>
2026-05-03 12:27:54 +0530

    Refactor code structure for improved readability and maintainability

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {registerUser,loginuser,logoutUser,refreshAccessToken} from "../controllers/user.controller.js"
+import {registerUser,loginuser,logoutUser,refreshAccessToken, updateProfile, getcurrentuser, getUserWatchHistory, userchannelprofile, updateUserCoverimage, updateUserAvatar, changePassword} from "../controllers/user.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
@@ -27,5 +27,12 @@ router.route("/register").post(
 router.route("/login").post(loginuser);
 router.route("/logout").post(verifyJWT,logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-
+router.route("/change-password").post(verifyJWT,changePassword);
+router.route("/update-avatar").post(verifyJWT,upload.single("avatar"),updateUserAvatar);
+router.route("/update-coverimage").post(verifyJWT,upload.single("coverImage"),updateUserCoverimage);
+router.route("/c/:username").get(verifyJWT,
+userchannelprofile);
+router.route("/watch-history").get(verifyJWT,getUserWatchHistory);
+router.route("/current-user").get(verifyJWT,getcurrentuser);
+router.route("/update-user").put(verifyJWT,updateProfile);
 export default router;
